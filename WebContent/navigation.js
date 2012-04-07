748be691cec88dac5596facfbbe31d7b1d6677a6
Michael Palotas <palotas@yahoo.com>
2012-04-07 23:04:24 +0200

    new site design

--- WebContent/navigation.js
+++ WebContent/navigation.js
@@ -1,3 +1,4 @@
+/*
 var cText = '<div id="navigation">';
 	cText += '<ul>';
 	cText += '<li>';
@@ -9,3 +10,23 @@ var cText = '<div id="navigation">';
 	cText += '</ul>';
 	cText += '</div>';
 document.write(cText);
+*/
+
+
+var cText = '<div id="dolphincontainer">';
+		cText += '	<div id="dolphinnav">';
+cText += '<ul>';
+cText += '<li><a href="./index.html" title=Home"><span>Home</span></a></li>';
+cText += '<li><a href="" title="About Us"><span>About Us</span></a></li>';
+cText += '<li><a href="" title="Services" onclick="this.className="current"><span>Services</span></a></li>';
+cText += '<li><a href="" title="css website navigation menus"><span>Services</span></a></li>';
+cText += '</ul>';
+cText += '</div>';
+cText += '</div>';
+
+document.write(cText);
+
+
+/*
+<a class="home" href="javascript:void(0);" id="home" onclick="this.className='current';">home</a>
+*/

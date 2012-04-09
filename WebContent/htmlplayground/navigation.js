f310b22e1dcd707fd45325fde14dfa131cffb12c
Michael Palotas <palotas@yahoo.com>
2012-04-09 20:26:35 +0200

    good version, added table row highlighting and completed training.hml with dates

---
+++ WebContent/htmlplayground/navigation.js
@@ -0,0 +1,32 @@
+/*
+var cText = '<div id="navigation">';
+	cText += '<ul>';
+	cText += '<li>';
+	cText += '<a href="#home">Home</a>';
+	cText += '</li>';
+	cText += '<li><a href="./services.html">Services</a></li>';
+	cText += '<li><a href="./contact.html">Contact</a></li>';
+	cText += '<li><a href="#about">About</a></li>';
+	cText += '</ul>';
+	cText += '</div>';
+document.write(cText);
+*/
+
+
+var cText = '<div id="dolphincontainer">';
+		cText += '	<div id="dolphinnav">';
+cText += '<ul>';
+cText += '<li><a href="./index.html" title=Home"><span>Home</span></a></li>';
+cText += '<li><a href="" title="About Us"><span>About Us</span></a></li>';
+cText += '<li><a href="" title="Services" onclick="this.className="current"><span>Services</span></a></li>';
+cText += '<li><a href="" title="css website navigation menus"><span>Services</span></a></li>';
+cText += '</ul>';
+cText += '</div>';
+cText += '</div>';
+
+document.write(cText);
+
+
+/*
+<a class="home" href="javascript:void(0);" id="home" onclick="this.className='current';">home</a>
+*/

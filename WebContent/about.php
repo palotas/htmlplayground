00bdd52bcd5d0834c881af83b3fef08af0e3407c
Michael Palotas <mpalotas@ebay.com>
2012-06-14 17:51:07 +0100

    initial checkin

---
+++ WebContent/about.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="description" content="Homepage of Michael Palotas" />
+<meta name="keywords" content="Michael Palotas, Palotas, Head of Quality, Head of Quality Engineering, eBay, Selenium, Testautomation, Iqnite, Swiss Testing Day" />
+<title>PALOTAS.org - About Me</title>
+
+    <LINK href="main.css" rel="stylesheet" type="text/css">
+    <LINK href="navigation.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Averia+Serif+Libre' rel='stylesheet' type='text/css'>
+    
+	<!-- Google Analytics -->
+	<script type="text/javascript">
+
+  	var _gaq = _gaq || [];
+  	_gaq.push(['_setAccount', 'UA-30332697-1']);
+  	_gaq.push(['_trackPageview']);
+
+  	(function() {
+    	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+ 	 })();
+
+	</script><!-- END Google Analytics -->
+
+  	<!-- util functions -->
+  	<script src="./utils.js"></script> 
+
+</head>
+<body>
+
+<div id="container">
+
+	<div id="banner"></div>
+	<div id="bannercontent" onclick="location.href='index.html';"></div>
+	
+	<div style="clear:both;"></div>
+	
+	<script type="text/javascript" src="./includes/navigation.js"></script>
+	
+	
+	<div style="clear:both;"></div>
+	
+	
+	
+	<div id="content">		
+		
+		<div class="maincontainer">
+		<p>
+		<b>CONTACT</b><br/>
+		Michael Palotas<br>
+		Gerbiweg 2<br>
+		8853 Lachen SZ<br>
+		SWITZERLAND<br>
+		Email: info@gridfusion.net<br>
+		<p>DISCLAIMER: The content and opinions on this website are exclusively representing Michael Palotas.</p>
+		</p>
+		
+		<!-- contact form is included here -->
+		<? include("mailform.php"); ?>
+		</div><!--END maincontainer-->
+		
+	</div> <!-- CONTENT -->
+	
+
+	
+		
+	<script type="text/javascript" src="./includes/footer.js">  </script>
+	<script>setActiveTab();</script>
+	
+
+</div>
+
+</body>
+</html>

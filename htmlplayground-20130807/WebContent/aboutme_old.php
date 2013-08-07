75db65830dc5aa4c60bdeb43b9db5377a77d9219
Palotas <mpalotas@lm-zrh-00357967.corp.ebay.com>
2013-08-07 17:41:59 +0200

    initial commit

---
+++ htmlplayground-20130807/WebContent/aboutme_old.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="description" content="Gridfusion - Test Automation Consulting" />
+<meta name="keywords" content="Michael Palotas, test consulting, selenium consulting, head of quality engineering, java, page objects, open source, 
+innovation, test engineering, conferences, swiss testing day, conference board, palotas, Selenium, webdriver, webdriver training, 
+Selenium Training, Selenium Consulting, test automation, gridfusion, agile testing, agile automation " />
+
+<title>GRIDFUSION.net - About</title>
+
+    <LINK href="main.css" rel="stylesheet" type="text/css">
+    <LINK href="tables.css" rel="stylesheet" type="text/css">
+    <LINK href="navigation.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Varela' rel='stylesheet' type='text/css'>
+    
+<!-- GOOGLE ANALYTICS -->  	
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-30699229-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+  	<!-- util functions -->
+  	<script src="./utils.js"></script> 
+
+<!-- GOOGLE ANALYTICS -->  	
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-30699229-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</head>
+<body>
+
+<div id="container">
+
+	<div id="banner"></div>
+	<div id="bannercontent" onclick="location.href='index.html';"></div>
+	
+	<div style="clear:both;"></div>
+	
+	<script type="text/javascript" src="./includes/navigation.js"></script>
+
+	
+	<div style="clear:both;"></div>
+	
+	
+	
+	<div id="content">		
+		
+		<div class="maincontainer">
+			
+			<p><b>Company background</b></p>
+			<p>With many years in the software industry in lead roles at Ericsson, Nortel Networks, Intel and eBay, 
+				Michael Palotas founded GRIDFUSION to share his passion and knowledge and to help organizations to 
+				implement open source test automation.
+			</p>
+			<p>
+				Michael had a direct influence on Selenium Grid, which was developed and open sourced within his team at eBay.  
+				Selenium Grid has become a standard component of the most popular open source browser automation framework.
+			</p>
+			<p>	
+				Next to expert knowledge in test automation, GRIDFUSION also has a strong network of partners for implementing full scale solutions and providing related services. 
+			</p>			
+		</div>
+		
+		<div class="maincontainer">
+			<p><b>Michael Palotas upcoming public events:</b></p>
+			<table>
+								<tr height="10" class="tableHeader">
+									<td width="90px"><b>Date</b></td>
+									<td width="250x"><b>Event</b></td>
+									<td width="490px"><b>Topic</b></td>
+									<td width="40px"><b>Location</b></td>																										
+								</tr>
+								<tr class="bg0">
+									<td>Jan 8</td>	
+									<td>Fachhochschule Heilbronn</a></td>	
+									<td>Guest Lecture</td>	
+									<td>Heilbronn</td>	
+								</tr>
+								<tr class="bg1">
+									<td>Jan 22</td>	
+									<td><a href="http://www.sigs-datacom.de/oop2013/oop2013.html" target="_blank">OOP 2013</a></td>	
+									<td>Mobile Test Automation at eBay</td>	
+									<td>Munich</td>	
+								</tr>	
+								<tr class="bg0">
+									<td>Feb 5</td>	
+									<td><a href="http://www.digicomp.ch/opentuesday" target="_blank">Open Tuesday</a></td>	
+									<td>Die Selenium Toolfamilie und ihr Einsatz im Web- und Mobile Automatisierungumfeld</td>	
+									<td>Zürich</td>		
+								</tr>	
+								<tr class="bg1">
+									<td>Mar 21</td>	
+									<td>Siemens Global Testing Conference</td>	
+									<td>TBD</td>	
+									<td>Erlangen</td>	
+								</tr>	
+								<tr class="bg0">
+									<td>Mar 30</td>	
+									<td><a href="http://www.codefest.ru" target="_blank">Codefest Russia</a></td>	
+									<td>Unfolding the Swiss Army Knife - The skilfull tester of the 21st century</td>	
+									<td>Novosibirsk</td>	
+								</tr>
+								<tr class="bg1">
+									<td>Apr 18</td>	
+									<td>Novomatic Test Conference</td>	
+									<td>TBD</td>	
+									<td>Vienna</td>	
+								</tr>
+								
+								
+								
+			</table>
+		</div>
+		
+
+
+		<div class="maincontainer" style="height: 450px">
+			<p style="font-size: 14px"><b>Michael Palotas - Professional Summary</b></p>
+					
+			<p>Dipl. Ing (FH) Nachrichtentechnik (Electrical Engineering)</p>
+			<p>Stanford University 2011<br>
+			British Columbia Institute of Technology 2002<br>
+			University of Applied Sciences Dieburg 1998</p>
+			</p>
+			
+			<p>
+			<div class="cv_years">Founder / Principal Consultant</div> 
+			<div class="cv_description">GRIDFUSION Software Solutions, Lachen CH<br/></div>
+			</p>
+			
+			<p>
+			<div class="cv_years">Head of Quality Engineering Europe (2008-current)</div> 
+			<div class="cv_description">eBay International, Zürich CH</div>
+			</p>
+			
+			<p>
+			<div class="cv_years">Head of Product Development (2007-2008)</div>
+			<div class="cv_description">eBay, Berlin DE</div>
+			</p>
+
+			<p>
+			<div class="cv_years">Senior Manager Quality Assurance (2004-2007)</div>
+			<div class="cv_description">eBay, Berlin DE</div>
+			</p>
+								
+			<p>
+			<div class="cv_years">Senior Software Engineer (2000-2003)</div>
+			<div class="cv_description">Intel, Vancouver CA</div>
+			</p>
+			
+			<p>
+				
+			<div class="cv_years">Network Test Engineer (1999-2000)</div>
+			<div class="cv_description">Nortel Networks, Winnipeg CA
+			</div>
+			</p>
+
+			<p>
+			<div class="cv_years">Software Design Engineer (1998-1999)</div>
+			<div class="cv_description">Ericsson Eurolab , Nürnberg DE</div>
+			</p>
+			
+		</div><!--END maincontainer-->
+		
+	</div> <!-- CONTENT -->
+	
+
+	
+<script type="text/javascript" src="./includes/footer.js">  </script>
+		
+	<script>setActiveTab();</script>
+</div>
+
+</body>
+</html>

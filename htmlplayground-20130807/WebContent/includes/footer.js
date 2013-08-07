1669ee3c095cbfc2ccc55049dbaa2f2eca4991a4
Palotas <mpalotas@lm-zrh-00357967.corp.ebay.com>
2013-08-07 17:46:08 +0200

    delete files

--- htmlplayground-20130807/WebContent/includes/footer.js
+++
@@ -1,4 +0,0 @@
-document.write("<div id=footer>GRIDFUSION Software Solutions - Michael Palotas - Gerbiweg 2 - 8853 Lachen SZ, Switzerland - info@gridfusion.net</div>");
-//document.write("<div class=partnerlogo><img src=./images/gridlogo.jpg></div>");
-//document.write("<div class=partnerlogo><img src=./images/gridlogo.jpg></div>");
-//document.write("<div class=partnerlogo><img src=./images/gridlogo.jpg></div>");
--- htmlplayground-20130807/WebContent/includes/regAlert.js
+++
@@ -1,1 +0,0 @@
-alert("Dieses Training wird in Kooperation mit DIGICOMP durchgeführt.\nBitte verwenden Sie den Code Selenium4GridFusion bei der Registrierung.");
